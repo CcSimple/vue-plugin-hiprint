25d2a19abf3c7451a764f8868e6998839b6929cc
Cc-Mac <840054486@qq.com>
2022-04-22 11:04:48 +0800

    调整优化 更新到npm包b0.0.17

--- vue.config.js
+++ vue.config.js
@@ -7,23 +7,6 @@ module.exports = {
   outputDir: 'demo',
   assetsDir: 'static',
   productionSourceMap: false,
-  chainWebpack: config => {
-    // 打包时 修改package.json 的dependencies
-    config.plugin('define').tap(args => {
-      if (process.env.NODE_ENV === 'production') {
-        const pkgPath = path.join(__dirname, './package.json');
-        let pkg = fs.readFileSync(pkgPath);
-        pkg.dependencies = {
-          "ant-design-vue": "^1.7.8",
-          "core-js": "^3.6.5",
-          "vue": "^2.5.11",
-          "concurrent-tasks": "^1.0.7",
-          "vue-ls": "^4.0.0"
-        }
-      }
-      return args
-    })
-  },
   configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
