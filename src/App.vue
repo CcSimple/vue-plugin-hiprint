061ade1a27c22787a33b18265b1399a3d757eb48
Cc-Mac <840054486@qq.com>
2022-04-28 17:17:45 +0800

    demo 修改页眉/页脚线 样式

--- src/App.vue
+++ src/App.vue
@@ -67,4 +67,32 @@ export default {
     content: url("~@/assets/logo.png");
   }
 }
+// 修改 页眉/页脚线 样式
+.hiprint-headerLine, .hiprint-footerLine {
+  border-color: red !important;
+}
+
+.hiprint-headerLine:hover, .hiprint-footerLine:hover {
+  border-top: 3px dashed red !important;
+}
+
+.hiprint-headerLine:hover:before {
+  content: "页眉线";
+  left: 50%;
+  position: relative;
+  background: #FFFF;
+  top: -12px;
+  color: red;
+  font-size: 12px;
+}
+
+.hiprint-footerLine:hover:before {
+  content: "页脚线";
+  left: 50%;
+  position: relative;
+  color: red;
+  background: #FFFF;
+  top: -12px;
+  font-size: 12px;
+}
 </style>
