76106f7b017a655f5302c2e50b38994931b13005
Canim <840054486@qq.com>
2023-01-12 20:18:12 +0800

    action pubBeta

---
+++ scripts/change-version.js
@@ -0,0 +1,21 @@
+let path = require('path')
+const fs = require("fs");
+const pkgPath = path.join(__dirname, '../package.json');
+let pkg = JSON.parse(fs.readFileSync(pkgPath));
+let version = pkg.version;
+console.log(version);
+const vList = version.split(".");
+const vEnd = vList[vList.length - 1];
+if (`${vEnd}`.includes('beta')) {
+  let pre = parseInt(vList[vList.length - 1]);
+  let cur = 'beta' + (parseInt(vList[vList.length - 1].split('-')[1].replace(/[^\d./]/g, '') || 0) + 1);
+  version = `${vList[0]}.${vList[1]}.${pre}-${cur}`;
+  console.log('=> ', version);
+} else {
+  let cur = parseInt(vList[vList.length - 1]) + 1;
+  version = `${vList[0]}.${vList[1]}.${cur}-beta1`;
+  console.log('=> ', version);
+}
+pkg.version = version;
+let pkgStr = JSON.stringify(pkg, null, 2);
+fs.writeFileSync(pkgPath, pkgStr);
